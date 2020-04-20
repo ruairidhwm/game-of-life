732ec920da1334ffb543139fa5d449ce88258fa4
Ruairidh Wynne-McHardy <ruairidhwm@gmail.com>
2020-04-20 12:25:35 +0100

    ✨ feat: Logic to add our grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+
+const numberOfRows: number = 50;
+const numberOfColumns: number = 50;
+
+const [grid, setGrid] = useState(() => {
+  const rows = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    rows.push(Array.from(Array(numberOfColumns), () => 0));
+  }
+});
 
 const App: React.FC = () => {
   return <div>Hi</div>;
