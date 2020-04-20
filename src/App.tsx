8263b9b043f99579c2ace7b35cedb37f88063f16
Ruairidh Wynne-McHardy <ruairidhwm@gmail.com>
2020-04-20 12:41:16 +0100

    ♻️ refactor: Put hooks inside the function body

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
 import React, { useState } from "react";
 import produce from "immer";
 
-const numberOfRows: number = 50;
-const numberOfColumns: number = 50;
-
-/**
- * @description
- * Creates the array we need to store our
- * grid in. We set 0 as being dead, and will
- * set 1 as being alive.
- *
- * By default we start everything as dead.
- */
-const [grid, setGrid] = useState(() => {
-  const rows = [];
-  for (let i = 0; i < numberOfRows; i++) {
-    rows.push(Array.from(Array(numberOfColumns), () => 0));
-  }
-
-  return rows;
-});
-
-const [running, setRunning] = useState(false);
-
 const App: React.FC = () => {
+  const numberOfRows: number = 50;
+  const numberOfColumns: number = 50;
+
+  /**
+   * @description
+   * Creates the array we need to store our
+   * grid in. We set 0 as being dead, and will
+   * set 1 as being alive.
+   *
+   * By default we start everything as dead.
+   */
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numberOfRows; i++) {
+      rows.push(Array.from(Array(numberOfColumns), () => 0));
+    }
+
+    return rows;
+  });
+
+  const [running, setRunning] = useState(false);
+
   return (
     <>
       <button onClick={() => setRunning(!running)}>
